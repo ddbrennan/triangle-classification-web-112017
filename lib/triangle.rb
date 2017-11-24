c973fdb313194513c2f1e88a41f3c9b22df1c366
ddbrennan <ddbren@gmail.com>
2017-11-24 17:23:26 -0500

    add triangle class and triangleerror

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :sides
+
+  def initialize(side_a, side_b, side_c)
+    @sides = [side_a, side_b, side_c]
+  end
+
+  def kind
+    if !valid?
+      raise TriangleError
+    elsif sides.uniq.length == 1
+      :equilateral
+    elsif sides.uniq.length == 2
+      :isosceles
+    elsif sides.uniq.length == 3
+      :scalene
+    end
+  end
+
+  def valid?
+    length = sides.all? {|side| side > 0}
+    side_sum = sides.inject(0){|sum,x| sum + x }
+    size = sides.all? {|side| side < side_sum - side}
+    length && size
+  end
+
+end
+
+
+
+class TriangleError < StandardError
+ # triangle error code
 end
